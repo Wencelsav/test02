15b771851083048b21c5011dfc64d1161590ff84
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2017-12-16 22:09:45 +0300

    Update 001.cpp

--- 001.cpp
+++ 001.cpp
@@ -1,64 +1,41 @@
 #include <iostream>
-#include <utility>
-#include <string>
 #include <sstream>
-#include <limits>
 using namespace std;
+int reverse (int *a, int n){
+	for (int i = 0, j = n - 1; i < j; ++i, --j) {
 
-void selectionSort(int *a, int n)
-{
-	for (int i = 0; i < n / 2; i++)
-	{
-		swap(a[i], a[n - i - 1]);
+		int t = a[i];
+		a[i] = a[j];
+		a[j] = t;
 	}
-}
-
-bool read_numbers(int *a, int n)
-{
-	string string;
-	getline(cin, string);
-	istringstream stream(string);
-	bool success = true;
-	for (int i = 0; i < n; ++i) 
-	{
-		if (!(stream >> a[i])) 
-		{
-			success = false;
-			break;
-		}
-	}
-	if (stream >> n)
-	{
-		success = false;
-		
-	}
-
-	return success;
-}
-
-int main() {
-	int n;
-	int *a;
-	cin >> n;
-	if (n>0)
-	{
-		cin.get();
-		a = new int[n];
-		if (read_numbers(a, n)) {
-			selectionSort(a, n);
-			for (int i = 0; i < n; i++) {
-				cout << a[i] << " ";
-			}
-		}
-		else {
-			cout << "An error has occured while reading input data.";
-		}
-	}
-	else {
-		cout << "An error has occured while reading input data.";
-	}
-	delete [] a;
-	cin.ignore(numeric_limits<streamsize>::max(), '\n');
 	return 0;
 }
-
+bool read(int * a, unsigned int size) {
+	string stroka;
+	getline(cin, stroka);
+	istringstream stream(stroka);
+	for (unsigned int i = 0; i < size; ++i) {
+		if (!(stream >> a[i])) {
+			return false;
+		}
+	}
+	return true;
+}
+int main() {
+	int n = 0;
+		if ((cin >> n) && (n > 1)) {
+			string s;
+			getline(cin, s);
+			istringstream stream(s);
+			int * a = new int[n];
+			if (read(a, n)) {
+				reverse(a, n);
+				for (int i = 0; i < n; ++i)
+					cout << " " << a[i];
+			}
+			else { cout << "An error has occured while reading input data." << endl; }
+			delete[] a;
+		}
+		else { cout << "An error has occured while reading input data." << endl; }
+		return 0;
+	}
