e0490d9a3b1170e9b2496ae0ca828d5885872b16
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2017-12-16 23:04:29 +0300

    Update 001.cpp

--- 001.cpp
+++ 001.cpp
@@ -1,43 +1,44 @@
 #include <iostream>
 #include <sstream>
 using namespace std;
-int reverse (int *a, int n){
+int reverse(int *a, int n) {
 	for (int i = 0, j = n - 1; i < j; ++i, --j) {
 
-		int t = a[i];
-		a[i] = a[j];
-		a[j] = t;
+		swap(a[i], a[j]);
 	}
 	return 0;
 }
-bool read(int * a, int n) {
-	string stroka;
-	int a_count = 0;
-	getline(cin, stroka);
-	istringstream stream(stroka);
-	for (int i = 0; i < n; ++i) {
-		if (!(stream >> a[i])) {
-			return false;
+bool read(int * a, int n)
+{
+	string mas;
+	int array_count = 0;
+	getline(cin, mas);
+	istringstream stream(mas);
+	for (int i=0; i < n; i++)
+	{
+		if (stream >> a[i])
+		{
+			array_count++;
 		}
-	}if (a_count == n && stream.eof()) return true;
+	}
+	if (array_count == n && stream.eof()) return true;
 	else return false;
-	return true;
 }
 int main() {
 	int n = 0;
-		if ((cin >> n) && (n > 1)) {
-			string s;
-			getline(cin, s);
-			istringstream stream(s);
-			int * a = new int[n];
-			if (read(a, n)) {
-				reverse(a, n);
-				for (int i = 0; i < n; ++i)
-					cout << " " << a[i];
-			}
-			else { cout << "An error has occured while reading input data." << endl; }
-			delete[] a;
+	if ((cin >> n) && (n > 1)) {
+		string s;
+		getline(cin, s);
+		istringstream stream(s);
+		int * a = new int[n];
+		if (read(a, n)) {
+			reverse(a, n);
+			for (int i = 0; i < n; ++i)
+				cout << " " << a[i];
 		}
 		else { cout << "An error has occured while reading input data." << endl; }
-		return 0;
+		delete[] a;
 	}
+	else { cout << "An error has occured while reading input data." << endl; }
+	return 0;
+}
