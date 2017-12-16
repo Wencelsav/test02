b9249664ebbbbe0822351521045b696cc592ba36
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2017-12-16 22:52:45 +0300

    Update 002.cpp

--- 002.cpp
+++ 002.cpp
@@ -66,3 +66,61 @@ int main() {
 	delete[] a;
 	return 0;
 }
+#include "stdafx.h"
+#include <iostream>
+#include <sstream>
+using namespace std;
+int reverse (int *a, int n){
+	for (int i = 0, j = n - 1; i < j; ++i, --j) {
+
+		int t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+}
+bool read(int * a, unsigned int n) {
+	string stroka;
+	int a_count = 0;
+	getline(cin, stroka);
+	istringstream stream(stroka);
+	for (unsigned int i = 0; i < n; ++i) {
+		if (!(stream >> a[i])) {
+			return false;
+		}
+	}if (a_count == n && stream.eof()) return true;
+	else return false;
+	return true;
+}
+bool shift(int &number_shift)
+{
+	string str;
+	getline(cin, str);
+	istringstream stream(str);
+	if (!(stream >> number_shift))
+	{
+		return false;
+	}
+	else
+	{
+		if (stream.eof()) { return true; }
+		else { return false; }
+	}
+}
+int main() {
+	int n = 0, number_shift;
+		if ((cin >> n) && (n > 1)) {
+			string s;
+			getline(cin, s);
+			istringstream stream(s);
+			int * a = new int[n];
+			if (read(a, n)&& shift(number_shift)) {
+				reverse(a, n);
+				for (int i = 0; i < n; ++i)
+					cout << " " << a[i];
+			}
+			else { cout << "An error has occured while reading input data." << endl; }
+			delete[] a;
+		}
+		else { cout << "An error has occured while reading input data." << endl; }
+		return 0;
+	}
